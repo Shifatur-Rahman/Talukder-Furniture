58f5655cc993980907b6a6513f0e86431efe46fa
Shifatur-Rahman <rjnovel28@gmail.com>
2025-10-08 12:22:58 +0600

    status add

---
+++ src/data/format.ts
@@ -0,0 +1,20 @@
+
+export const ironShelfProducts = [ 
+
+
+
+
+]
+
+
+
+
+
+export const ironShelfMain = [...ironShelfProducts].slice( 
+  0,
+  9
+);
+
+export const allIronShelf = [ 
+  ...ironShelfProducts, 
+]; 
---
+++ src/data/ironShelf.ts
@@ -0,0 +1,108 @@
+
+export const ironShelfProducts = [
+
+    {
+    id: 1,
+    delay: "0s",
+    title: "Iron Shelf", 
+    imgSrc: "/images/category/diningroom/iron-shelf/01.jpg", 
+    hoverImg: "/images/category/diningroom/iron-shelf/01.jpg", 
+    price: 100,
+    oldPrice: null, 
+    sale: null,
+    swatches: [
+      {
+        label: "Light Blue", 
+        bgClass: "bg-light-blue",
+        img: "/images/category/diningroom/iron-shelf/01.jpg", 
+      },
+      {
+        label: "Light Blue",
+        bgClass: "bg-light-blue-2",
+        img: "/images/category/diningroom/iron-shelf/01.jpg",
+      }, 
+    ],
+    filterBrands: ["Nike", "Louis Vuitton", "Zalando", "Adidas"], 
+    inStock: true,
+    filterColor: ["Beige", "Orange", "Green", "Black", "Purple", "Dark Blue"],
+    filterCategory: ["Bags", "Clothing", "Women", "Shoes", "Uncategorized"],
+    filterSizes: ["XS", "L", "XL", "2XL", "3XL"],
+    tabFilterOptions2: ["New Arrivals", "On Sale"],
+    tabFilterOptions: ["Bottoms", "Skirts", "Dresses", "Sale"],
+  },
+
+  {
+    id: 2,
+    delay: "0s",
+    title: "Iron Shelf", 
+    imgSrc: "/images/category/diningroom/iron-shelf/02.jpg", 
+    hoverImg: "/images/category/diningroom/iron-shelf/02.jpg", 
+    price: 100,
+    oldPrice: null, 
+    sale: null,
+    swatches: [
+      {
+        label: "Light Blue", 
+        bgClass: "bg-light-blue",
+        img: "/images/category/diningroom/iron-shelf/02.jpg", 
+      },
+      {
+        label: "Light Blue",
+        bgClass: "bg-light-blue-2",
+        img: "/images/category/diningroom/iron-shelf/02.jpg",
+      }, 
+    ],
+    filterBrands: ["Nike", "Louis Vuitton", "Zalando", "Adidas"], 
+    inStock: true,
+    filterColor: ["Beige", "Orange", "Green", "Black", "Purple", "Dark Blue"],
+    filterCategory: ["Bags", "Clothing", "Women", "Shoes", "Uncategorized"],
+    filterSizes: ["XS", "L", "XL", "2XL", "3XL"],
+    tabFilterOptions2: ["New Arrivals", "On Sale"],
+    tabFilterOptions: ["Bottoms", "Skirts", "Dresses", "Sale"],
+  },
+
+  {
+    id: 3,
+    delay: "0s",
+    title: "Iron Shelf", 
+    imgSrc: "/images/category/diningroom/iron-shelf/03.jpg", 
+    hoverImg: "/images/category/diningroom/iron-shelf/03.jpg", 
+    price: 100,
+    oldPrice: null, 
+    sale: null,
+    swatches: [
+      {
+        label: "Light Blue", 
+        bgClass: "bg-light-blue",
+        img: "/images/category/diningroom/iron-shelf/03.jpg", 
+      },
+      {
+        label: "Light Blue",
+        bgClass: "bg-light-blue-2",
+        img: "/images/category/diningroom/iron-shelf/03.jpg",
+      }, 
+    ],
+    filterBrands: ["Nike", "Louis Vuitton", "Zalando", "Adidas"], 
+    inStock: true,
+    filterColor: ["Beige", "Orange", "Green", "Black", "Purple", "Dark Blue"],
+    filterCategory: ["Bags", "Clothing", "Women", "Shoes", "Uncategorized"],
+    filterSizes: ["XS", "L", "XL", "2XL", "3XL"],
+    tabFilterOptions2: ["New Arrivals", "On Sale"],
+    tabFilterOptions: ["Bottoms", "Skirts", "Dresses", "Sale"],
+  },
+
+
+]
+
+
+
+
+
+export const ironShelfMain = [...ironShelfProducts].slice( 
+  0,
+  9
+);
+
+export const allIronShelf = [ 
+  ...ironShelfProducts, 
+]; 
