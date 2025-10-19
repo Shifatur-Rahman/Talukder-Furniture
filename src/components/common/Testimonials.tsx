ab372570982f1e3ec1fd876a7415a6ddc7d32f1b
Shifatur-Rahman <rjnovel28@gmail.com>
2025-10-19 11:56:14 +0600

    status add

--- src/components/common/Testimonials.tsx
+++ src/components/common/Testimonials.tsx
@@ -86,7 +86,7 @@ export default function Testimonials({
                               />
                             </g>
                             <defs>
-                              <clipPath>
+                              <clipPath> 
                                 <rect
                                   width={20}
                                   height={20}
